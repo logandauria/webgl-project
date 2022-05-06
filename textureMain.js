3b9656f6613ef271961dde5fbef071b1563786cd
Logan D'Auria <lxd1644@rit.edu>
2022-05-06 02:01:07 -0400

    inner tex fix

--- textureMain.js
+++ textureMain.js
@@ -41,13 +41,13 @@ function setUpTextures(){
     gl.bindTexture(gl.TEXTURE_2D, orbTexture);
     
     // load the actual image
-    var worldImage = document.getElementById ('world-texture')
+    var orbImage = document.getElementById ('orb-texture')
         
     // bind the texture so we can perform operations on it
     gl.bindTexture (gl.TEXTURE_2D, orbTexture);
         
     // load the texture data
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, worldImage.width, worldImage.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, worldImage);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, orbImage.width, orbImage.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, orbImage);
         
     // set texturing parameters
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
